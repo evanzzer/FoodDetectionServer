1e50c77182afa78ccafc1a5b18ca7a15a1ce7535
EvAnZz <56476347+evanzzer@users.noreply.github.com>
2023-05-10 21:36:42 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/microsoft/onnxruntime/releases/download/v1.8.1/onnxruntime-linux-x64-1.8.1.tgz
+tar -zxvf onnxruntime-linux-x64-1.8.1.tgz
+export ONNXRUNTIME_DIR=$(pwd)/onnxruntime-linux-x64-1.8.1
+export LD_LIBRARY_PATH=$ONNXRUNTIME_DIR/lib:$LD_LIBRARY_PATH
