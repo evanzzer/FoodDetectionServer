743e9dcb8108297428590738fb8806819093582d
EvAnZz <56476347+evanzzer@users.noreply.github.com>
2023-05-10 21:52:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install python3-opencv wget
 wget https://github.com/microsoft/onnxruntime/releases/download/v1.8.1/onnxruntime-linux-x64-1.8.1.tgz
 tar -zxvf onnxruntime-linux-x64-1.8.1.tgz
 export ONNXRUNTIME_DIR=$(pwd)/onnxruntime-linux-x64-1.8.1
