df57d185d8ccbb65d562a0b881c2155eb0d5ce8a
EvAnZz <56476347+evanzzer@users.noreply.github.com>
2023-05-15 09:20:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install wget libgl1 -y
+apt-get update && apt-get install wget libgl1-mesa-glx ffmpeg libsm6 libxext6 -y --no-install-recommends
 wget https://github.com/microsoft/onnxruntime/releases/download/v1.8.1/onnxruntime-linux-x64-1.8.1.tgz -O onnxruntime-linux-x64-1.8.1.tgz
 rm -rf onnxruntime-linux-x64-1.8.1
 tar -zxvf onnxruntime-linux-x64-1.8.1.tgz
