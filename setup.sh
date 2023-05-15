354a396c3077e3423d78cb529f14bb3c21937a38
EvAnZz <56476347+evanzzer@users.noreply.github.com>
2023-05-15 09:13:54 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install wget -y
+apt-get update && apt-get install wget libgl1 -y
 wget https://github.com/microsoft/onnxruntime/releases/download/v1.8.1/onnxruntime-linux-x64-1.8.1.tgz -O onnxruntime-linux-x64-1.8.1.tgz
 rm -rf onnxruntime-linux-x64-1.8.1
 tar -zxvf onnxruntime-linux-x64-1.8.1.tgz
