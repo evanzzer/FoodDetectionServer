9b0d8b56dfd7f6557104914f0bff3d9f1faaaf13
EvAnZz <56476347+evanzzer@users.noreply.github.com>
2023-05-15 09:08:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install wget python3-opencv -y
-pip install opencv-python
+apt-get update && apt-get install wget -y
 wget https://github.com/microsoft/onnxruntime/releases/download/v1.8.1/onnxruntime-linux-x64-1.8.1.tgz -O onnxruntime-linux-x64-1.8.1.tgz
 rm -rf onnxruntime-linux-x64-1.8.1
 tar -zxvf onnxruntime-linux-x64-1.8.1.tgz
